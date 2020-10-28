e0147c6e9869b8ce9ba4e379cd443d0fa25a407c
Christoffer A Traeen <christoffer.andersen@live.com>
2020-10-28 01:04:17 +0100

    Change to process time

--- app/core/debug_tools/timer.py
+++ app/core/debug_tools/timer.py
@@ -1,6 +1,7 @@
 # timer.py
 import time
 
+
 class Timer:
     def __init__(self, countdown):
         self._start_time = None
@@ -8,12 +9,12 @@ class Timer:
 
     def start(self):
         if self._start_time is None:
-            self._start_time = time.clock()
+            self._start_time = time.process_time()
             print(f"Countdown: {self.countdown} seconds")
 
     def current(self):
-        return int((time.clock() - self._start_time))
-    
+        return int((time.process_time() - self._start_time))
+
     def is_running(self):
         if self._start_time is not None:
             return True
@@ -21,20 +22,20 @@ class Timer:
 
     def stop(self):
         if self._start_time is not None:
-            elapsed_time = time.clock() - self._start_time
+            elapsed_time = time.process_time() - self._start_time
             self._start_time = None
             print(f"Elapsed time: {elapsed_time:0.5f} seconds")
 
     def is_expired(self):
         if self._start_time is not None:
-            if (time.clock() - self._start_time) >= self.countdown:
+            if (time.process_time() - self._start_time) >= self.countdown:
                 return True
             return False
 
     def restart(self):
         if self._start_time is not None:
             print("Restarting!")
-            self._start_time = time.clock()
+            self._start_time = time.process_time()
 
     def renew(self, extra_time):
         if self._start_time is not None:
@@ -42,12 +43,9 @@ class Timer:
             self._start_time += extra_time
 
 
-
-
-
 if __name__ == "__main__":
 
-    counter = MyCounter(countdown=5)
+    counter = MyCounter(countdown=2)
     counter.start()
     while True:
         if counter.is_expired():
